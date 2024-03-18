a0832b48d466733410bbb0d2d3b1b2dca9653436
Raven Hendricks <ravenkbeats@gmail.com>
2024-03-18 18:41:35 -0400

    copied code from prior project

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
+import MainSection from "./components/MainSection";
+import NavBar from "./components/NavBar";
 import './App.css'
 
 function App() {
 
   return (
-    <>
-      
-    </>
+     <div id="container">
+        <NavBar />
+        <MainSection />
+    </div> 
   )
 }
 
