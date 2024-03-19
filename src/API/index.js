03268f13908114e456d305a4f9e59f81290b0a3d
Raven Hendricks <ravenkbeats@gmail.com>
2024-03-18 20:55:02 -0400

    created new function to add new player

--- src/API/index.js
+++ src/API/index.js
@@ -12,3 +12,24 @@ export async function getCurrentPlayerById(playerId) {
     const result = await response.json();
     return result.data.player;
 }
+
+export async function addNewPlayer(playerId){
+    try {
+        const response = await fetch(`${API_URL}/${playerId}`,
+        {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                name: //name field value from form
+                breed: //breed field from form value
+                status: // status field value from form
+            }),
+        });
+        const result = await response.json();
+        console.log(result.data.player);
+    } catch (error) {
+        console.error(error)
+    }
+}
