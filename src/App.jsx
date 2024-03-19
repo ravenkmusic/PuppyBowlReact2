f1db4117aee28621d84de4dfa20124eb0120d12d
Raven Hendricks <ravenkbeats@gmail.com>
2024-03-19 12:58:36 -0400

    imported search bar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import MainSection from "./components/MainSection";
 import NavBar from "./components/NavBar";
+import SearchBar from "./components/SearchBar";
 import './App.css'
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
   return (
      <div id="container">
         <NavBar />
+        <SearchBar />
         <MainSection />
     </div> 
   )
