0095025a3c57a129e0cc6a0c5a578c345b07eb53
Raven Hendricks <ravenkbeats@gmail.com>
2024-03-19 10:03:37 -0400

    added link and route to new player form

--- src/components/MainSection.jsx
+++ src/components/MainSection.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import AllPlayers from './AllPlayers';
 import SinglePlayer from './SinglePlayer';
+import NewPlayerForm from './NewPlayerForm';
 
 export default function MainSection() {
     return (
             <Routes>
                 <Route path='/' element={<AllPlayers />} />
                 <Route path = '/players/:id' element={<SinglePlayer />} />
+                <Route path = '/newplayer' element={<NewPlayerForm />} />
             </Routes>
     )
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,6 +4,7 @@ export default function NavBar (){
     return (
         <div id="navbar">
             <Link to="/">Home</Link>
+            <Link to = "/newplayer">Create A New Player!</Link>
         </div>
     )
 }
